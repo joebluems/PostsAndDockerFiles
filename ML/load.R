0044ff58222b6d2f3bc358b371a251968f4666ed
joebluems <joeblue@Joe-Blues-MacBook-Pro-2.local>
2018-07-26 15:31:24 -0700

    adding ML

---
+++ ML/load.R
@@ -0,0 +1,3 @@
+install.packages("jug", repos = "http://cran.us.r-project.org")
+install.packages("caret", repos = "http://cran.us.r-project.org")
+install.packages("randomForest", repos = "http://cran.us.r-project.org")
